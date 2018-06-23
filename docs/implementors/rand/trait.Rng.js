16df76bfaf768e83fd3336101f29d6a8ed8f99a9
Andrei Maximov <andrei.maximov@gmail.com>
2018-06-22 18:09:27 -0700

    Update docs
    Commit: d1ffc3df2bc22cc9bee171ac3dc382c59ca86302

--- docs/implementors/rand/trait.Rng.js
+++ docs/implementors/rand/trait.Rng.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["rand"] = [];
+implementors["usrnet"] = [{text:"impl <a class=\"trait\" href=\"rand/trait.Rng.html\" title=\"trait rand::Rng\">Rng</a> for <a class=\"struct\" href=\"usrnet/core/rng/struct.MockRng.html\" title=\"struct usrnet::core::rng::MockRng\">MockRng</a>",synthetic:false,types:["usrnet::core::rng::MockRng"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
